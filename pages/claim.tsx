f489b01aaeb89bae4659b41340d5ef7de82b552d
--replace-all <al.gadzhalov@gmail.com>
2022-12-03 21:50:00 +0200

    Changed: code cleanup

--- pages/claim.tsx
+++ pages/claim.tsx
@@ -2,14 +2,11 @@ import { useWeb3React } from "@web3-react/core";
 import Head from "next/head";
 import Link from "next/link";
 import Account from "../components/Account";
-import PolygonClaimView from "../components/polygon/PolygonClaimView";
-import EthereumNetwork from "../components/ethereum/EthereumNetwork";
 import Menu from "../components/Menu";
 import NativeCurrencyBalance from "../components/NativeCurrencyBalance";
 import SwitchNetwork from "../components/SwitchNetwork";
 import { ETHEREUM_BRIDGE_GOERLI, POLYGON_BRIDGE_MUMBAI } from "../constants";
 import useEagerConnect from "../hooks/useEagerConnect";
-import EthereumClaimView from "../components/ethereum/EthereumClaimView";
 import ClaimContainer from "../components/claim/ClaimContainer";
 
 function Claim() {
@@ -43,9 +40,6 @@ function Claim() {
             <Menu page="Claim" />
             <SwitchNetwork />
             <ClaimContainer />
-            {/* {chainId == 5 && (<EthereumClaimView bridgeAddress={ETHEREUM_BRIDGE_GOERLI} />)}
-            {chainId == 80001 && (<PolygonClaimView bridgeAddress={POLYGON_BRIDGE_MUMBAI} />)} */}
-            
           </section>
         )}
       </main>
