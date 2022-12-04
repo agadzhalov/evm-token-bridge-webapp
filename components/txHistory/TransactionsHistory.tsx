0bf92d814b38ed2379a5038a23eb8bf39294c974
--replace-all <al.gadzhalov@gmail.com>
2022-12-04 16:33:05 +0200

    Added: Spinner for TxHistory onLoad

--- components/txHistory/TransactionsHistory.tsx
+++ components/txHistory/TransactionsHistory.tsx
@@ -16,19 +16,24 @@ import Moment from 'react-moment';
 import { GOERLI_CHAIN_ID, MUMBAI_CHAIN_ID } from "../../constants/networks";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 const TransactionsHistory = () => {
     const { account, library, chainId } = useWeb3React();
     const { getUrl } = useTxsHistory();
     const [txs, setTxs] = useState<any | undefined>();
+    const [isTxsLoading, setIsTxsLoading] = useState<boolean | undefined>(false);
 
     useEffect(() => {
+        setIsTxsLoading(true);
         axios
             .get(getUrl())
             .then((response) => {
+                setIsTxsLoading(false);
                 setTxs(response.data.result)
             })
             .catch(error => {
+                setIsTxsLoading(false);
                 console.log(error);
             })
     }, [chainId])
@@ -51,34 +56,39 @@ const TransactionsHistory = () => {
 
     return (
         <div className="results-form">
-            <DataTable 
+            {isTxsLoading && (
+                <ProgressSpinner style={{ width: '50px', height: '50px' }} strokeWidth="2" fill="var(--surface-ground)" animationDuration=".5s" />
+            )}
+            {!isTxsLoading && (
+                <DataTable 
                     value={txs} 
                     paginator 
                     responsiveLayout="scroll"
                     stripedRows
                     currentPageReportTemplate="Showing {first} to {last} of {totalRecords}" rows={10} rowsPerPageOptions={[10,20,50]}
                 >
-                <Column field="timeStamp" header="Date" className="link" body={(tx) => (
-                    <Moment unix>{tx.timeStamp}</Moment>
-                )}> 
-                </Column>
-                <Column field="hash" header="Hash" className="link" body={(tx) => (
-                    link(tx.hash, "Transaction") 
-                )}> 
-                </Column>
-                <Column field="from" header="From" className="link" body={(tx) => (
-                    link(tx.from, "Account") 
-                )}> 
-                </Column>
-                <Column field="to" header="To" className="link" body={(tx) => (
-                    link(tx.to, "Account") 
-                )}> 
-                </Column>
-                <Column field="gasPrice" header="Gas Price" className="link" body={(tx) => (
-                    ethers.utils.formatEther(tx.gasPrice)
-                )}> 
-                </Column>
-            </DataTable>
+                    <Column field="timeStamp" header="Date" className="link" body={(tx) => (
+                        <Moment unix>{tx.timeStamp}</Moment>
+                    )}> 
+                    </Column>
+                    <Column field="hash" header="Hash" className="link" body={(tx) => (
+                        link(tx.hash, "Transaction") 
+                    )}> 
+                    </Column>
+                    <Column field="from" header="From" className="link" body={(tx) => (
+                        link(tx.from, "Account") 
+                    )}> 
+                    </Column>
+                    <Column field="to" header="To" className="link" body={(tx) => (
+                        link(tx.to, "Account") 
+                    )}> 
+                    </Column>
+                    <Column field="gasPrice" header="Gas Price" className="link" body={(tx) => (
+                        ethers.utils.formatEther(tx.gasPrice)
+                    )}> 
+                    </Column>
+                </DataTable>
+            )}
         </div>
     );
 };
